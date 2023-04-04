ba4b271deca365028ee97b3c9423d06d544b0e99
sandile05 <106535890+sandile05@users.noreply.github.com>
2023-04-04 09:56:13 +0200

    sumAll attempt

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,17 @@
-const sumAll = function() {
+const sumAll = function(first, second) {
+    if (!Number.isInteger(first) || !Number.isInteger(second)) return "ERROR";
+
+    if(first < 0 || second < 0){
+        return "ERROR"
+    }
+
+    let num = 0;
+
+    for(i = first; first <= second; i++) {
+        num += i;
+    }
+
+    return num;
 
 };
 
